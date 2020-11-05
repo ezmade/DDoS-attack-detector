ff720231a216218e74ec8d8365ef433f569c8649
Anton Boyko <montegof@mail.ru>
2020-11-05 16:51:07 +0500

    Fixed isssue with ML module

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request
 
 from ddos_detector import predict_ddos_attack
+from app_components import get_files_from_root
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,11 +16,13 @@ def model(model_id=0):
 
 @app.route('/about')
 def about():
-    return 'The about page'
+    return render_template('about.html')
 
-@app.route('/learning')
+@app.route('/learning', methods=['GET'])
 def learning():
-    return render_template('learning.html')
+    models = get_files_from_root('ddos-attack-detector\\Model', '.sav')
+    message = 'Выбери модель'
+    return render_template('learning.html', models=models, message=message)
 
 @app.route('/classification')
 def classification():
@@ -29,8 +32,8 @@ def classification():
 def classificate_data():
     file = request.files.get('file')
     try:
-        prediction = predict_ddos_attack('DDoS-attack-detector\Model\AdamClassifier.sav', file)
-        return str(prediction), 200
+        prediction = predict_ddos_attack('ddos-attack-detector\\Model\\AdamClassifier.sav', file)
+        return str(prediction[0]), 200
     except:
         return '',400
 
--- ddos_detector.py
+++ ddos_detector.py
@@ -6,8 +6,8 @@ from numpy import reshape
 
 def label_encoding(data):
     columns_to_encode = list(data.select_dtypes(include=['category', 'object']))
-    print(data.dtypes) # Prints each column d_type
-    print(columns_to_encode) # Prints categorical features
+    # print(data.dtypes) # Prints each column d_type
+    # print(columns_to_encode) # Prints categorical features
 
     le = LabelEncoder()
     for feature in columns_to_encode:
@@ -21,17 +21,19 @@ def label_encoding(data):
 
 def load_model(path):
     loaded_model = load(open(path, 'rb'))
-    print(loaded_model.coefs_)
-    print(loaded_model.loss_)
+    # print(loaded_model.coefs_)
+    # print(loaded_model.loss_)
 
     return loaded_model
 
 
-def predict_ddos_attack(model, data):
-
+def predict_ddos_attack(model_path, file):
+    model = load_model(model_path)
+    data = read_csv(file)
+    enconded_data = label_encoding(data)
     try:
         print('good')
-        features = data[['Highest Layer', 'Transport Layer', 'Source IP', 'Dest IP', 'Source Port',
+        features = enconded_data[['Highest Layer', 'Transport Layer', 'Source IP', 'Dest IP', 'Source Port',
                         'Dest Port', 'Packet Length', 'Packets/Time']]
         print(features)
     except:
@@ -41,11 +43,3 @@ def predict_ddos_attack(model, data):
     prediction = model.predict(features)
     print('good3')
     return prediction
-
-def main():
-    model = load_model('DDoS-attack-detector\\Model\\Model.sav')
-    data = read_csv('DDoS-attack-detector\\Data\\TestingData.csv')
-    print(predict_ddos_attack(model, data))
-
-if __name__=='__main__':
-    main()
